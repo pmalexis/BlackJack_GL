8c4ea97bd9619d6f5e086d80b817339dc33001cc
Draxxo <killer76520@gmail.com>
2016-10-13 14:34:07 +0200

    Euh rajout du controleur ? il aimait pas notre travail il s'est barré...

---
+++ src/controller/Controleur.java
@@ -0,0 +1,15 @@
+package controller;
+
+import model.MoteurBlackjack;
+
+public class Controleur {
+
+	private MoteurBlackjack moteur;
+	
+	public Controleur() {
+	}
+	
+	public MoteurBlackjack getMoteur() {
+		return this.moteur;
+	}
+}
